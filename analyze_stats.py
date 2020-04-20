eaef3b6ffda1a30dae228c297fd8f43db1eb5e39
Erik Bjäreholt <erik@bjareho.lt>
2020-04-20 15:18:41 +0200

    feat: improved plotting for use on website stats page

--- analyze_stats.py
+++ analyze_stats.py
@@ -1,4 +1,4 @@
-from datetime import timezone
+from datetime import datetime, timezone
 import matplotlib.pyplot as plt
 import pandas as pd
 import click
@@ -39,12 +39,14 @@ def test_load():
 
 @click.command()
 @click.option("--column")
+@click.option("--save")
 @click.option("--since", type=click.DateTime(["%Y-%m-%d"]))
-def main(column=None, since=None):
-    n_plots = 3
+def main(column: str = None, save: str = None, since: datetime = None):
+    n_plots = 2
 
     df = _load_data()
     df = df.resample("1D").mean()
+    df = df.interpolate(method="time")  # interpolate missing dates
 
     if column:
         df = df[column]
@@ -52,6 +54,7 @@ def main(column=None, since=None):
     if since:
         df = df.truncate(before=since)
 
+    plt.figure(figsize=(8, 2.5 * n_plots))
     ax1 = plt.subplot(n_plots, 1, 1)
     df.plot(ax=ax1, title=column)
     ax1.set_title("Cumulative")
@@ -59,24 +62,34 @@ def main(column=None, since=None):
     ax1.legend()
 
     if n_plots >= 2:
-        ax2 = plt.subplot(n_plots, 1, 2, sharex=ax1)
-        df_d = df.diff()
-        df_d = df_d[df_d > 0]  # Filter out the crazy outlier
-        df_d.plot(ax=ax2)
-        ax2.set_title("Per day")
-        ax2.set_ylim(0)
-
-    if n_plots >= 3:
-        ax = plt.subplot(n_plots, 1, 3, sharex=ax1)
+        ax = plt.subplot(n_plots, 1, 2)  # , sharex=ax1)
         df_w = df.diff()
         df_w = df_w[df_w > 0]  # Filter out the crazy outlier
+        df_w = df_w.resample("1D").mean()
         df_w = df_w.rolling("7D").mean() * 7
         df_w.plot(ax=ax)
-        ax.set_title("Per week")
+        ax.set_title("Per week (rolling)")
         ax.set_ylim(0)
+        ax.legend()
 
-    plt.show()
+    # if n_plots >= 2:
+    #     ax2 = plt.subplot(n_plots, 1, 2, sharex=ax1)
+    #     df_d = df.diff()
+    #     df_d = df_d[df_d > 0]  # Filter out the crazy outlier
+    #     df_d.plot(ax=ax2)
+    #     ax2.set_title("Per day")
+    #     ax2.set_ylim(0)
+
+    plt.tight_layout()
+    # plt.subplots_adjust(hspace=0.10)
+
     # print(df)
+
+    if save:
+        plt.savefig(save)
+        # plt.show()
+    else:
+        plt.show()
 
 
 if __name__ == "__main__":
